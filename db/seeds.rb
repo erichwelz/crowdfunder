61835bad7e4c4523b3d7bfdb8e5dc90c0d9974a3
Erich Welz <erichwelz@gmail.com>
2013-12-02 18:40:06 -0500

    faker project seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+
+Project.create({
+  title: Faker::Commerce.product_name,
+  description: Faker::Lorem.sentence(word_count = 20),
+  goal_in_dollars: (rand * 10000).to_i,
+  start_date: Date.today,
+  finish_date: Date.today + 20,
+  owner_id: 1,
+  public_date: Date.today + 1
+
+})
+end
