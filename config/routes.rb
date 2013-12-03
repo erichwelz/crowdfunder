96e6519732fd914080278ecba4773985544308ff
Pat Hann <pathann@gmail.com>
2013-12-03 10:35:22 -0500

    added session user controller.  signup works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Crowdfunder::Application.routes.draw do
 
+
+	resources :sessions
+
+	resources :users 
+
+  get "logout" => "sessions#destroy", :as => "logout"
+	get "login" => "sessions#new", :as => "login"
+	get "signup" => "users#new", :as => "signup"
+
   resources :projects 
   resources :categories, only: [:new, :create]
   root :to => "projects#index"
