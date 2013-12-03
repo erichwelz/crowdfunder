13c6d86bb51b8a9c4f00ddfa4458f1fae9aedf11
Pat Hann <pathann@gmail.com>
2013-12-03 12:19:04 -0500

    added full size header and created user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ def new
 		@user = User.new(user_params)
 
 		if @user.save
+			auto_login(@user)
 			# UserMailer.welcome_email(@user).deliver
 			redirect_to projects_url, :notice => "signed up!"
 		else
