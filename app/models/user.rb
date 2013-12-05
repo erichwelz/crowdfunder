657abce34f9ad66e99785ba650abd024297606b9
Pat Hann <pathann@gmail.com>
2013-12-04 19:37:08 -0500

    tidied up some things with links and css

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
 
 	has_attached_file :avatar, :styles => { 
-		  :mini => "37x37>",
+		  :mini => "30x30>",
 	   	:medium => "300x300>",
 	   	:thumb => "100x100#" }, :default_url => '/images/:attachment/missing_:style.png'
 	   	
